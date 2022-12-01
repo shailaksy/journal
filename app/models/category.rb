b0608ca4e80f890bfd638301b75c6685b557d681
shailaksy <shailaksy@gmail.com>
2022-12-01 20:17:00 +0800

    added controllers

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,24 +1,3 @@
 class Category < ApplicationRecord
-
-    # test "should get index" do
-    #     get categories_path
-    #     assert_response :success
-    # end
-
-    # test "should get new" do
-    #     get new_categories_path
-    #     assert_response :success
-    # end
-
-    # test "should get create" do
-    #     post create_category_path, params: { category: { name: "test", details: "details" } }
-    #     assert_response :redirect
-    # end
-
-    # test "should get create fail" do
-    #     post create_category_path, params: { category: { name: "test", details: "details" } }
-    #     assert_response_not :success
-    # end
-
     validates :name, :details, presence: true
 end
