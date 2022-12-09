6a1611e4aa0c2399ccc4cd4f01a2fa73eb7980f7
shailaksy <shailaksy@gmail.com>
2022-12-09 19:42:06 +0800

    added styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index]
     def index
-        # @user = current_user
-        # @categories = @user.categories
+        @user = current_user
+        @categories = Category.all
     end
 end
